61b861bd0ea25242e0b6d735e9668c112eab7dac
harshadbirajdar <hbirajdar1@gmail.com>
2022-06-12 11:47:28 +0530

    cors issue fix

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ cloudinary.config({
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://harshadfood.vercel.app/"],
     credentials: true,
   })
 );
